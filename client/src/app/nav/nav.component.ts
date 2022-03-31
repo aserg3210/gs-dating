56ea4e7d169e2811b9719d040fc24116b6c478ca
aserg3210@gmail.comm <aserg3210@gmail.com>
2022-04-01 02:30:30 +0300

    navbar was  added

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  model: any = {}
+  loggedIn: boolean;
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+  }
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: e => {
+        console.log(e);
+      }
+    });
+  }
+  logout(){
+    this.loggedIn = false;
   }
 
 }
