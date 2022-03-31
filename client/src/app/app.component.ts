aff37b44a99cbc471406ea7e6eceba7926268d4a
aserg3210@gmail.comm <aserg3210@gmail.com>
2022-03-31 03:24:04 +0300

    base client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'client';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => {
+        this.users = response;
+        console.log(this.users);
+      },
+      error: e=> {
+        console.log(e);
+      }
+    });
+  }
+
 }
